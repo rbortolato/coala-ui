ed9e55f0da54c549b747ff6d552d02f13fbfc1b6
rbortolato <bortolato@live.com>
2023-09-10 21:43:03 -0300

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  important: "#root",
   theme: {
     extend: {
       gridTemplateColumns: {
